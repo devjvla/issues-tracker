1f5fb1a21a5b483340028234d5e856ab91c2c148
devjvla <dev.jvla@gmail.com>
2024-06-07 13:44:10 +0800

    Add styling to navbar link based on current path returned by usePathname()

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,25 +1,29 @@
+"use client"
 import Link from "next/link"
+import { usePathname } from "next/navigation";
 import React from "react"
 
 // Icons
 import { PiBugBeetleLight } from "react-icons/pi";
 
 const NavBar = () => {
+	const currentPath = usePathname();
+
 	const NAVBAR_STYLES = {
 		branding: "flex font-extrabold text-green-600 items-center",
-		links: "font-light text-zinc-500 hover:text-green-600 transition-colors"
+		classes: "font-light text-zinc-500 hover:text-green-600 transition-colors"
 	}
 
 	const NAVBAR_LINKS = [
-		{ label: "Dashboard", href: "/dashboard", className: NAVBAR_STYLES.links },
-		{ label: "Issues", href: "/issues", className: NAVBAR_STYLES.links },
+		{ label: "Dashboard", href: "/dashboard", classes: currentPath == "/dashboard" ? "text-green-600 font-light" : NAVBAR_STYLES.classes },
+		{ label: "Issues", href: "/issues", classes: currentPath == "/issues" ? "text-green-600 font-light" : NAVBAR_STYLES.classes },
 	]
 
   return (
 		<nav className="flex space-x-12 border-b mb-5 p-8">
 			<Link href="/" className={NAVBAR_STYLES.branding}><PiBugBeetleLight className="mr-2 text-2xl"/>IssueTracker</Link>
 			<ul className="flex space-x-6">
-				{ NAVBAR_LINKS.map((link, index) => ( <li key={index}><Link href={link.href} className={link.className}>{link.label}</Link></li> ))}
+				{ NAVBAR_LINKS.map((link, index) => ( <li key={index}><Link href={link.href} className={link.classes}>{link.label}</Link></li> ))}
 			</ul>
 		</nav>
 	)
